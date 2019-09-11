0ec443d619bdd93ed287cfd111424b271cdd5ab5
thdsilva <thiagosilva9587@gmail.com>
2019-09-11 20:44:35 -0300

    Adiconada linha que faltava

--- app/src/main/java/br/uninove/imcacademia/MainActivity.java
+++ app/src/main/java/br/uninove/imcacademia/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         this.txtAltura = findViewById(R.id.txtAltura);
         this.txtPeso = findViewById(R.id.txtPeso);
         this.txtResultado = findViewById(R.id.txtResultado);
+        this.imgResultado = findViewById(R.id.imgResultado);
 
   /*  }
     public void btnVerificarOnClick(View view){
